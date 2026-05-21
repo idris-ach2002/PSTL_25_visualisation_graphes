789e71e2712e7633fea7f9b57c8667f9d1cb6ed6
Chetouani Bilal <chetouanibilal04@gmail.com>
2026-05-21 12:23:11 +0200

    correction crash overflow pool

--- graph-application/graph-native/c/concurrent/Tools.c
+++ graph-application/graph-native/c/concurrent/Tools.c
@@ -1,15 +1,11 @@
 #include "Tools.h"
 
-int incr_or_max(_Atomic int* n, int max)
-{
+int incr_or_max(_Atomic int* n, int max) {
     int res = *n;
-    while ( ! atomic_compare_exchange_weak(n, &res, res+1) )
-    {
-        if ( res >= max ){
-            *n = max;
-            return max;
+    while (res < max) {
+        if (atomic_compare_exchange_weak(n, &res, res + 1)) {
+            return res;  // retourne l'index valide
         }
     }
-
-    return res;
+    return -1;  // limite atteinte
 }
